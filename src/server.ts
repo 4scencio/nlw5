3623a5cd54fe351d5c323a2c62979ea190863d4a
4scencio <alessandroascencio@protonmail.com>
2021-04-19 11:39:46 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Olá #NLW5 :D'
+    })
+})
+
+app.post('/msg', async (request, response) => {
+    const { message } = await request.body
+
+    return response.json({ message })
+})
+
+app.listen(3000, () => console.log('🚀 O servidor está rodando na porta 3000'))
