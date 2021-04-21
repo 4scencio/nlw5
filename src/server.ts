cfcd0e61ee40759a1002c1df33810ea30e2b9213
4scencio <alessandroascencio@protonmail.com>
2021-04-21 11:53:07 -0300

    Setting Repository e Controller

--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,8 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Olá #NLW5 :D'
-    })
-})
+import { routes } from './routes'
 
-app.post('/msg', async (request, response) => {
-    const { message } = await request.body
-
-    return response.json({ message })
-})
+app.use(routes)
 
 app.listen(3000, () => console.log('🚀 O servidor está rodando na porta 3000'))
