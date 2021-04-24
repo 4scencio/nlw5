a87b1c650cf056b0ebbb94217172a7d55e295eef
4scencio <alessandroascencio@protonmail.com>
2021-04-24 11:03:02 -0300

    Recebendo socket e-mail e text

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,4 @@
-import express from 'express'
-import { createServer } from 'http'
-import { Server, Socket } from 'socket.io'
-
-import path from 'path'
-
-import './database'
-import { routes } from './routes'
-
-const app = express()
-
-app.use(express.static(path.join(__dirname, '..', 'public')))
-app.set('views', path.join(__dirname, '..', 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-
-app.get('/pages/client', (request, response) => {
-    return response.render('html/client.html')
-})
-
-const http = createServer(app) // Criando Protocólo HTTP
-const io = new Server(http) // Criando Protocólo WebSocket
-
-io.on('connection', (socket: Socket) => {
-    console.log(`${socket.id} se conectou`)
-})
-
-app.use(express.json())
-
-app.use(routes)
+import { http } from './http'
+import './websockets/client'
 
 http.listen(3000, () => console.log('🚀 O servidor está rodando na porta 3000'))
